050b6da74fcbd121696c2626562818d15771d3cf
WilliamNHarvey <williamnharvey@gmail.com>
2019-09-23 21:12:21 -0400

    Initial Commit

---
+++ DesignSystem/DesignSystem.h
@@ -0,0 +1,19 @@
+//
+//  DesignSystem.h
+//  DesignSystem
+//
+//  Created by William Harvey on 2019-09-23.
+//  Copyright © 2019 PillPals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesignSystem.
+FOUNDATION_EXPORT double DesignSystemVersionNumber;
+
+//! Project version string for DesignSystem.
+FOUNDATION_EXPORT const unsigned char DesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignSystem/PublicHeader.h>
+
+
