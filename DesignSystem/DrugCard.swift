2a8f40ec9f168b0bba09c1fc7b28d239ec9b6372
WilliamNHarvey <williamnharvey@gmail.com>
2019-10-06 16:18:41 -0400

    Make DrugCard nib

---
+++ DesignSystem/DrugCard.swift
@@ -0,0 +1,69 @@
+//
+//  DrugCard.swift
+//  DesignSystem
+//
+//  Created by William Harvey on 2019-10-05.
+//  Copyright © 2019 PillPals. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class DrugCard: UIView {
+    @IBOutlet public var cardView: UIView!
+    @IBOutlet public weak var drugName: UILabel!
+    @IBOutlet public weak var drugTime: UILabel!
+    @IBOutlet public weak var lateLabel: UILabel!
+    @IBOutlet public weak var logButton: UIButton!
+    
+    @IBAction public func logDrug(_ sender: Any) {
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        //Bundle.main.loadNibNamed("DrugCard", owner: self, options: nil)
+//        let bundle = Bundle(for: type(of: self))
+//        let nib = UINib(nibName: "DrugCard", bundle: bundle)
+//        let nibView = nib.instantiate(withOwner: self, options: nil).first
+//        self.fixInView(nibView as? UIView)
+        
+        let view = UINib(nibName: "DrugCard", bundle: Bundle(for: type(of: self))).instantiate(withOwner: self, options: nil)[0] as! UIView
+
+        //view.frame = 
+
+        //view.autoresizingMask = [.flexibleWidth]
+        
+        self.addSubview(view);
+    }
+    
+    public func stretchToSuperView()
+    {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        //print(self.superview?.widthAnchor)
+        guard let superview = self.superview else {
+            print("Error! `superview` was nil – call `addSubview(view: UIView)` before calling `bindFrameToSuperviewBounds()` to fix this.")
+            return
+        }
+        NSLayoutConstraint(item: self.subviews[0], attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: superview, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0).isActive = true
+        self.heightAnchor.constraint(equalToConstant: 72).isActive = true
+        
+        
+        //self.superview?.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
+//        var d = Dictionary<String,UIView>()
+//        d["view"] = self
+//        for axis in ["H"] {
+//            let format = "\(axis):|[view]|"
+//            let constraints = NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: [:], views: d)
+//            self.superview?.addConstraints(constraints)
+//        }
+    }
+}
--- DesignSystem/Extensions.swift
+++ DesignSystem/Extensions.swift
@@ -28,3 +28,15 @@ extension UIColor {
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
 }
+
+extension UIView {
+    func fixInView(_ container: UIView!) -> Void{
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
